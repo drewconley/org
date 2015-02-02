9298dd8ccc2ff74ad6fd4c7ab9d60e0f1adef678
Drew Conley <drewconley@gmail.com>
2015-02-01 19:41:15 -0600

    add scss build

---
+++ _posts/2015-2-1-crafting-leadership.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Crafting Leadership"
+date:   2015-2-1
+---
+
+This is the beginning of a post called Crafting Leadership.
+
+
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('s', function() {
+    browserSync({
+        server: {
+            baseDir: "./_site"
+        },
+        files: ['css/**'], //NOTE: This watches the compiled version, not the _site public sheets
+        ghostMode: false
+    });
+});
+
+/* BUILDING */
+gulp.task('sass', function () {
+    gulp.src('frontend/scss/main.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        //.pipe(gulp.dest('_site/css'))
+        .pipe(gulp.dest('css')) //print compiled sheet in two places so nothing gets left out of a build
+        .pipe(reload({stream:true}));
+    return true;
+});
+
+
+gulp.task('build', ['sass']);
+
+gulp.task('watch', function() {
+    gulp.watch('frontend/scss/**/*.scss', ['sass']);
+});
+
+
+gulp.task('default',['build', 'watch']);
